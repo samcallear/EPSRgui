818e344775fb9e07d06e2c2af4088162e1c7bcdb
samcallear <sam.callear@stfc.ac.uk>
2017-02-04 18:27:22 +0000

    trying to find mac exeDir_ equivalent

--- src/epsrproject_mainwin.cpp
+++ src/epsrproject_mainwin.cpp
@@ -2918,7 +2918,14 @@ void MainWindow::openEPSRmanual()
 
 void MainWindow::openEPSRguiManual()
 {
+#ifdef Q_OS_MAC
+    QMessageBox msgBox;
+    msgBox.setText(exeDir_.path());
+    msgBox.exec();
+    QString guimanual = exeDir_.path()+"/SharedServices/EPSRgui Manual.pdf";
+#else
     QString guimanual = exeDir_.path()+"/EPSRgui Manual.pdf";
+#endif
     QDesktopServices::openUrl(QUrl("file:///"+guimanual, QUrl::TolerantMode));
 }
 
