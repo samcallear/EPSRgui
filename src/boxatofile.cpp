3a403894de9c0fc142cd7813991a08a9fc9c7816
samcallear <sam.callear@stfc.ac.uk>
2017-02-14 15:21:01 +0000

    added changeato to updateAtoBox.. to reformat ready for readAtoFileDetails()

--- src/boxatofile.cpp
+++ src/boxatofile.cpp
@@ -1147,6 +1147,22 @@ void MainWindow::on_updateAtoFileButton_clicked(bool checked)
     QFile::remove(workingDir_+atoFileName_);
     QFile::rename(workingDir_+"temp.txt", workingDir_+atoFileName_);
 
+    //run changeato to reformat in EPSR style (req'd as use QRegExp for ecoredcore in readAtoFileBoxDEtails())
+     QDir::setCurrent(workingDir_);
+     QString atoBaseFileName = atoFileName_.split(".",QString::SkipEmptyParts).at(0);
+
+     processEPSR_.setProcessChannelMode(QProcess::ForwardedChannels);
+ #ifdef _WIN32
+     processEPSR_.start(epsrBinDir_+"changeato.exe", QStringList() << workingDir_ << "changeato" << atoBaseFileName);
+ #else
+     processEPSR_.startDetached(epsrBinDir_+"changeato", QStringList() << workingDir_ << "changeato" << atoBaseFileName);
+ #endif
+     if (!processEPSR_.waitForStarted()) return;
+     processEPSR_.write("e\n");
+     processEPSR_.write("\n");
+     processEPSR_.write("y\n");
+     if (!processEPSR_.waitForFinished(60000)) return;
+
     readAtoFileBoxDetails();
 
     messageText_ += "\nbox .ato file updated\n";
